5dea0cb3afe86704fb058f2209e415c955e202e0
Pathan Afarojkha <123102309+Mrkhan012@users.noreply.github.com>
2025-05-08 11:22:52 +0530

    text_style for app

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -5,5 +5,5 @@ class AppTextStyles {
   static final title = GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w600);
   static final subtitle = GoogleFonts.poppins(fontSize: 16, color: Colors.grey[700]);
   static final price = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.green[700]);
-  static final button = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white);
+  static final button = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white );
 }
