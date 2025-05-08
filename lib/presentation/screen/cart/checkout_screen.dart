c93a22e5e7f913795fbd7ea3855244b234011740
Pathan Afarojkha <123102309+Mrkhan012@users.noreply.github.com>
2025-05-08 11:29:11 +0530

    checkout itme view

--- lib/presentation/screen/cart/checkout_screen.dart
+++ lib/presentation/screen/cart/checkout_screen.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce/core/utils/colros.dart';
-import 'package:ecommerce/presentation/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
